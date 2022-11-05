22c38644a1565632a73e6d9cfcf7a6bd384d53a8
CharlotteStrickland <charlottemarie81@gmail.com>
2022-11-05 00:05:24 -0400

    completed solution 1-5

--- solution1.py
+++ solution1.py
@@ -1,1 +1,15 @@
 # Write code for algorithm 1 below
+
+def count_down(n):
+    
+    #base case
+    if n==0:
+        return
+    
+    #recursive case
+    else:
+        print(n)
+        count_down(n-1)
+
+n=8
+count_down(n)
--- solution5.py
+++ solution5.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 5 below
+
+def isPalindrome(word):
+    if len(word) < 2:
+        return True
+    else:
+        if word[0] == word[-1]:
+            return isPalindrome(word[1:-1])
+        else:
+            return False
+
+sample_word = 'radar'
+print(isPalindrome(sample_word))
